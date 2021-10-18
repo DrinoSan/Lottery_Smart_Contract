a0b7c8a1200724c6ecc76a9b1755509d6f3d124e
Sandrino <s.becirbegovic@gmail.com>
2021-10-18 23:01:47 +0200

    - Added True random number via chainlink VRF

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,11 +4,17 @@ pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
     address[] public players;
-    uint256 usdEntryFee;
+    uint256 public usdEntryFee;
+    address public recentWinner;
+    uint256 public randomness;
     AggregatorV3Interface internal ethPriceFeed;
+
+    uint256 public fee;
+    bytes32 public keyhash;
 
     enum LOTTERY_STATE {
         OPEN,
@@ -18,10 +24,18 @@ contract Lottery is Ownable {
 
     LOTTERY_STATE public lottery_state;
 
-    constructor(address _priceFeedAddress) public {
+    constructor(
+        address _priceFeedAddress,
+        address _vrf_coordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrf_coordinator, _link) {
         usdEntryFee = 50 * (10**18);
         ethPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
@@ -47,15 +61,35 @@ contract Lottery is Ownable {
 
     function end_lottery() public onlyOwner {
         // Pseude random number -> do not put in production friendly reminder :D
-        uint256(
-            keccak256(
-                abi.encodePacked(
-                    nonce, // is predicatble ( number of transactions )
-                    msg.sender, // is predicatble
-                    block.difficulty, // can be manipulated by the miners
-                    block.timestamp // is predicatble
-                )
-            )
-        ) % players.length;
+        // uint256(
+        //     keccak256(
+        //         abi.encodePacked(
+        //             nonce, // is predicatble ( number of transactions )
+        //             msg.sender, // is predicatble
+        //             block.difficulty, // can be manipulated by the miners
+        //             block.timestamp // is predicatble
+        //         )
+        //     )
+        // ) % players.length;
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You are not there yet!"
+        );
+        require(_randomness > 0, "No random number");
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        // Reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
     }
 }
